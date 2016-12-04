9c3bfdcef3c5ceca2527780e75e699ddfeb982b7
Valeriy Fedotov <valeriy.fedotov@gmail.com>
2016-12-04 23:57:25 +0300

    Add paintEvent and drawing of a circle

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QPainter>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +13,11 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    QPainter p(this); // Объект, с помощью которого будет выполняться всё рисование на виджете.
+
+    p.drawEllipse(QPointF(100, 100), 20, 20); // Рисуем эллипс в точке (100, 100) с радиусами 20 и 20 по X и Y.
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    // Для рисования поверх виджета нужно переопределить метод с именем PaintEvent.
+    // Очень важно, чтобы имя и сигнатура метода точно совпадали с теми, что написаны здесь,
+    // назвать метод по другому и добавлять аргументы нельзя.
+    // Описание метода см. в файле mainwindow.cpp
+    virtual void paintEvent(QPaintEvent *);
+
 private:
     Ui::MainWindow *ui;
 };
